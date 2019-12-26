a41165b4d4783b1a95b16368f556e2cfd381f0b2
Damien T.D.K <damientohin@gmail.com>
2019-12-26 10:05:06 +0100

    initial proj

---
+++ RuiDemo/RuiDemo/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using RuiDemo.Services;
+using RuiDemo.Views;
+
+namespace RuiDemo
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ RuiDemo/RuiDemo/Models/Item.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace RuiDemo.Models
+{
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+    }
+}
